244af728d23e12057014985b09bb0801c2f3b9a4
vagrant <vagrant@homestead>
2018-12-17 07:01:58 +0000

    fix

--- app/Admin/Controllers/DutyController.php
+++ app/Admin/Controllers/DutyController.php
@@ -321,7 +321,7 @@ class DutyController extends Controller
         $form->saved(function (Form $form) {
           //...
             admin_success('保存成功');
-            //return redirect('duty.index');
+            return redirect(route('duty.index'));
         });
         return $form;
     }
