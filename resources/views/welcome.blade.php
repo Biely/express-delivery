101ea875590020e56a071b0a77fdad294d266fef
vagrant <vagrant@homestead>
2019-01-02 15:49:41 +0000

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">进入系统</a>
                     @else
                         <a href="{{ route('login') }}">登录</a>
 
